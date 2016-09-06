2eaa62fae45b44791a1b7bd504673d70180f56a6
Devereux Henley <devereux@ksu.edu>
2016-09-06 13:57:05 -0500

    crooked snaek

--- snake.js
+++ snake.js
@@ -7,11 +7,14 @@ backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
 
-function snakeLink(direction,position,img,next) {
+var frameCounter = 0;
+
+function snakeLink(direction,position,img,prev,next) {
 	this.direction = direction;
 	this.position = position;
 	this.img = new Image(50,50);
 	this.img.src = img;
+	this.prev = prev;
 	this.next = next;
 }
 
@@ -27,22 +30,20 @@ function position(x, y) {
 	this.y = y;
 }
 
+var firstNode = new snakeLink(
+	new direction(false,true,false,false),
+	new position(30,30),
+	'snakebod.svg',
+	null,
+	null
+);
 var snake = {
-	head: new snakeLink(
-		new direction(false,true,false,false),
-		new position(0,0),
-		'snakebod.svg',
-		null
-	),
-  	length: 1
+	head: firstNode,
+	tail: firstNode,
+	length: 1
 }
 
-snake.head.next = new snakeLink(
-	snake.head.direction,
-	new position(snake.head.position.x, snake.head.position.y - 10),
-	'snakebod.svg',
-	null
-)
+for(i=0;i<10;i++){appendNewTail();}
 
 var input = new direction(false,false,false,false);
 
@@ -92,76 +93,140 @@ window.onkeyup = function(event) {
 	}
 }
 
-/**
- * @function loop
- * The main game loop.
- * @param{time} the current time as a DOMHighResTimeStamp
- */
+	/**
+	 * @function loop
+	 * The main game loop.
+	 * @param{time} the current time as a DOMHighResTimeStamp
+	 */
 function loop(newTime) {
-  var elapsedTime = newTime - oldTime;
-  oldTime = newTime;
+	var elapsedTime = newTime - oldTime;
+	oldTime = newTime;
 
-  update(elapsedTime);
-  render(elapsedTime);
+	update(elapsedTime);
+	render(elapsedTime);
 	
-	console.log(snake);
-  // Flip the back buffer
+	// Flip the back buffer
 	frontCtx.clearRect(0, 0, frontBuffer.width, frontBuffer.height);
-  	frontCtx.drawImage(backBuffer, 0, 0);
+	frontCtx.drawImage(backBuffer, 0, 0);
 
-  // Run the next loop
-  window.requestAnimationFrame(loop);
+	// Run the next loop
+	window.requestAnimationFrame(loop);
 }
 
-/**
- * @function update
- * Updates the game state, moving
- * game objects and handling interactions
- * between them.
- * @param {elapsedTime} A DOMHighResTimeStamp indicting
- * the number of milliseconds passed since the last frame.
- */
+	/**
+	 * @function update
+	 * Updates the game state, moving
+	 * game objects and handling interactions
+	 * between them.
+	 * @param {elapsedTime} A DOMHighResTimeStamp indicting
+	 * the number of milliseconds passed since the last frame.
+	 */
 function update(elapsedTime) {
 
-  // TODO: Spawn an apple periodically
-  // TODO: Grow the snake periodically
-  // TODO: Move the snake
-	var snakeNode = snake.head;
-	while(snakeNode.next != null) {
-		moveSnakeNode(snakeNode.next);
-		snakeNode.next.direction = snakeNode.direction;
-		snakeNode = snakeNode.next;
+	// TODO: Spawn an apple periodically
+	// TODO: Grow the snake periodically
+	// TODO: Move the snake
+	var snakeNode = snake.tail;
+	while(snakeNode.prev != null) {
+		moveSnakeNode(snakeNode);
+		if(frameCounter == 10) {
+			copyDirection(snakeNode.direction,snakeNode.prev.direction);
+		}
+		snakeNode = snakeNode.prev;
 	}
-	snake.head.direction = input;
+	if(frameCounter == 10) {
+		if(checkInput(input)) copyDirection(snake.head.direction,input);
+		frameCounter = 0;
+	}
+	else {
+		frameCounter++;
+	}
 	moveSnakeNode(snake.head);
-  // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  // TODO: Determine if the snake has eaten an apple
-  // TODO: Determine if the snake has eaten its tail
-  // TODO: [Extra Credit] Determine if the snake has run into an obstacle
 
+	 // TODO: Determine if the snake has moved out-of-bounds (offscreen)
+	// TODO: Determine if the snake has eaten an apple
+	// TODO: Determine if the snake has eaten its tail
+	// TODO: [Extra Credit] Determine if the snake has run into an obstacle
+
+}
+
+function appendNewTail() {
+	var newSnakeNode = new snakeLink(
+		new direction(snake.tail.direction.up, snake.tail.direction.down, snake.tail.direction.left, snake.tail.direction.right),
+		getNewTailPosition(),
+		'snakebod.svg',
+		snake.tail,
+		null
+	)
+
+	snake.tail.next = newSnakeNode;
+	snake.tail = newSnakeNode;
+	snake.length++;
+}
+
+function getNewTailPosition() {
+	if(snake.tail.direction.up) {
+		return new position(snake.tail.position.x, snake.tail.position.y + 10);
+	}
+	else if (snake.tail.direction.down) {
+		return new position(snake.tail.position.x, snake.tail.position.y - 10);
+	}
+	else if (snake.tail.direction.left) {
+		return new position(snake.tail.position.x + 10, snake.tail.position.y);
+	}
+	else if (snake.tail.direction.right) {
+		return new position(snake.tail.position.x - 10, snake.tail.position.y);
+	}
+}
+
+function copyDirection(target, source) {	
+	target.up = source.up;
+	target.down = source.down;
+	target.left = source.left;
+	target.right = source.right;
+	return target;
+}
+
+function copyObject(target, source) {
+	for (var prop in target) {
+		target[prop] = source[prop];
+	}
+	return target;
+}
+
+function checkInput(inputDirection) {
+	if(inputDirection.up || inputDirection.down || inputDirection.left || inputDirection.right) {
+		return true;
+	}
+	return false;
 }
 
 function moveSnakeNode(snakeNode) {
-	if(snakeNode.direction.up) snakeNode.position.y-=1;
-	else if (snakeNode.direction.down) snakeNode.position.y+=1;
-	else if (snakeNode.direction.left) snakeNode.position.x-=1;
-	else if (snakeNode.direction.right) snakeNode.position.x+=1;
+	if(snakeNode.direction.up) snakeNode.position.y -= 1;
+	else if (snakeNode.direction.down) snakeNode.position.y += 1;
+	else if (snakeNode.direction.left) snakeNode.position.x -= 1;
+	else if (snakeNode.direction.right) snakeNode.position.x += 1;
+	else console.log('movement error');
 }
 
-/**
-  * @function render
-  * Renders the current game state into a back buffer.
-  * @param {elapsedTime} A DOMHighResTimeStamp indicting
-  * the number of milliseconds passed since the last frame.
-  */
+function moveSnake(pos, snakeNode) {
+	snakeNode.pos
+}
+
+	/**
+	 * @function render
+	 * Renders the current game state into a back buffer.
+	 * @param {elapsedTime} A DOMHighResTimeStamp indicting
+	 * the number of milliseconds passed since the last frame.
+	 */
 function render(elapsedTime) {
-  	backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
+	backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 	var snakeNode = snake.head;
 	while(snakeNode != null) {
 		backCtx.drawImage(snakeNode.img, snakeNode.position.x, snakeNode.position.y);
 		snakeNode = snakeNode.next;
 	}	
-  // TODO: Draw the game objects into the backBuffer
+	// TODO: Draw the game objects into the backBuffer
 
 }
 
